ef9fb0c937b46412bc0127b32952647167a46c2f
Bryan Mayjor <bmayjor@gmail.com>
2013-06-19 14:10:05 -0700

    Added catman_webform_element() to move description above form fields

--- template.php
+++ template.php
@@ -10,35 +10,76 @@
  * on "catman".
  */
 
-
 /**
-* Replacement for theme_webform_element() to enable descriptions to come BEFORE the field to be filled out.
+* Replacement for theme_form_element().
 */
-// function catman_webform_element($variables) {
-//   $element = $variables['element'];
-//   $value = $variables['element']['#children'];
+function catman_webform_element($variables) {
+  // Ensure defaults.
+  $variables['element'] += array(
+    '#title_display' => 'before',
+  );
 
-//   $wrapper_classes = array(
-//     'form-item',
-//   );
-//   $output = '<div class="' . implode(' ', $wrapper_classes) . '" id="' . $element['#id'] . '-wrapper">' . "\n";
-//   $required = !empty($element['#required']) ? '<span class="form-required" title="' . t('This field is required.') . '">*</span>' : '';
+  $element = $variables['element'];
 
-//   if (!empty($element['#title'])) {
-//     $title = $element['#title'];
-//     $output .= ' <label for="' . $element['#id'] . '">' . t('!title: !required', array('!title' => filter_xss_admin($title), '!required' => $required)) . "</label>\n";
-//   }
+  // All elements using this for display only are given the "display" type.
+  if (isset($element['#format']) && $element['#format'] == 'html') {
+    $type = 'display';
+  }
+  else {
+    $type = (isset($element['#type']) && !in_array($element['#type'], array('markup', 'textfield'))) ? $element['#type'] : $element['#webform_component']['type'];
+  }
+  $parents = str_replace('_', '-', implode('--', array_slice($element['#parents'], 1)));
 
-//   if (!empty($element['#description'])) {
-//     $output .= ' <div class="description">' . $element['#description'] . "</div>\n";
-//   }
+  $wrapper_classes = array(
+   'form-item',
+   'webform-component',
+   'webform-component-' . $type,
+  );
+  if (isset($element['#title_display']) && $element['#title_display'] == 'inline') {
+    $wrapper_classes[] = 'webform-container-inline';
+  }
+  $output = '<div class="' . implode(' ', $wrapper_classes) . '" id="webform-component-' . $parents . '">' . "\n";
+  $required = !empty($element['#required']) ? '<span class="form-required" title="' . t('This field is required.') . '">*</span>' : '';
 
-//   $output .= '<div id="' . $element['#id'] . '">' . $value . '</div>' . "\n";
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+  $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . _webform_filter_xss($element['#field_prefix']) . '</span> ' : '';
+  $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . _webform_filter_xss($element['#field_suffix']) . '</span>' : '';
 
-//   $output .= "</div>\n";
+  switch ($element['#title_display']) {
+    case 'inline':
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+  if (!empty($element['#description'])) {
+$output .= ' <div class="description">' . $element['#description'] . "</div>\n";
+  }
 
-//   return $output;
-// }
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+  if (!empty($element['#description'])) {
+$output .= ' <div class="description">' . $element['#description'] . "</div>\n";
+  }
+
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  $output .= "</div>\n";
+
+  return $output;
+}
 
 
 /**
